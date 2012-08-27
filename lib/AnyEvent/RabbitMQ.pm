493dd04905c37031ace9d2c01a12da78756b2c1b
Tomas Doran <bobtfish@bobtfish.net>
2012-08-27 08:43:24 +0100

    Version 1.08

--- lib/AnyEvent/RabbitMQ.pm
+++ lib/AnyEvent/RabbitMQ.pm
@@ -29,7 +29,7 @@ use Net::AMQP::Common qw(:all);
 use AnyEvent::RabbitMQ::Channel;
 use AnyEvent::RabbitMQ::LocalQueue;
 
-our $VERSION = '1.07';
+our $VERSION = '1.08';
 
 Readonly my $DEFAULT_AMQP_SPEC
     => File::ShareDir::dist_dir("AnyEvent-RabbitMQ") . '/fixed_amqp0-8.xml';
